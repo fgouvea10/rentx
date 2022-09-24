74d5cefa7755f04d079763975c81f932e5e3c5cb
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-24 11:44:38 -0300

    feat: create categories repository

--- backend/src/routes/categories.routes.ts
+++ backend/src/routes/categories.routes.ts
@@ -1,26 +1,18 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 export const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-
-  Object.assign(category, {
+  categoriesRepository.create({
     name,
     description,
-    createdAt: new Date,
   })
 
-  categories.push(category);
-
-  return response.status(201).json({
-    success: true,
-    result: categories,
-  });
+  return response.status(201).send();
 });
