4d801052daddd1c6910826ca009e5670e1908d32
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-24 11:36:37 -0300

    feat: add category type

--- backend/src/routes/categories.routes.ts
+++ backend/src/routes/categories.routes.ts
@@ -1,18 +1,23 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from 'uuid';
+
+import { Category } from "../model/Category";
 
 export const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
-    id: uuidV4(),
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
-  });
+    createdAt: new Date,
+  })
+
+  categories.push(category);
 
   return response.status(201).json({
     success: true,
