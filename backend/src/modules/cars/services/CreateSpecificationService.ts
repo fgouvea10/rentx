65f344b53a992e473c29f1a83ffa89fc5c8080c3
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-25 20:15:43 -0300

    feat: create CreateSpecification service

---
+++ backend/src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,23 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationRepository";
+
+interface CreateSpecificationRequest {
+  name: string;
+  description: string;
+}
+
+export class CreateSpecificationService {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: CreateSpecificationRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists)
+      throw new Error("Specification already exists");
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
+  }
+}
