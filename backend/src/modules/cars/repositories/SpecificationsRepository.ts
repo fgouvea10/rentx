0ea93b5f84f12effb9a6723cb1f62667585ea85f
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-25 20:15:18 -0300

    feat: implement findByName in specification repository

--- backend/src/modules/cars/repositories/SpecificationsRepository.ts
+++ backend/src/modules/cars/repositories/SpecificationsRepository.ts
@@ -25,4 +25,11 @@ export class SpecificationsRepository implements ISpecificationsRepository {
 
     this.specifications.push(specification);
   }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+    return specification;
+  }
 }
