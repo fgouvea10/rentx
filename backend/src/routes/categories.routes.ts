614e50ed368d856df5c570ce43d1aef7acd37350
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-26 12:49:10 -0300

    refactor: categories list

--- backend/src/routes/categories.routes.ts
+++ backend/src/routes/categories.routes.ts
@@ -1,21 +1,14 @@
 import { Router } from "express";
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 export const categoriesRoutes = Router();
-
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const categories = categoriesRepository.list();
-
-  return response.status(200).json({
-    success: true,
-    result: categories,
-  });
+  return listCategoriesController.handle(request, response);
 });
