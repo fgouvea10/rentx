a5327f49092a4fe478999c4b78b2ca6227741bd8
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-26 15:23:37 -0300

    feat: add typeorm setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,17 @@
 import express from "express";
-import swaggerUi from 'swagger-ui-express';
+import swaggerUi from "swagger-ui-express";
 
 import { appRoutes } from "./routes";
-import swaggerFile from './swagger.json';
+import swaggerFile from "./swagger.json";
+
+import "./database";
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.use('/api/v1', appRoutes);
+app.use("/api/v1", appRoutes);
 
 app.listen(8000, () => console.log("Server is running"));
