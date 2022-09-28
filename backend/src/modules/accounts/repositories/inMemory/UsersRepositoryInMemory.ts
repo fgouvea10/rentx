cc5ba8d27ef35dcdbcfecaf7faaa7a7475911211
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-28 19:54:04 -0300

    feat: create users repository in memory

---
+++ backend/src/modules/accounts/repositories/inMemory/UsersRepositoryInMemory.ts
@@ -0,0 +1,37 @@
+import { UserDTO } from "../../dtos/user";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create({
+    name,
+    email,
+    password,
+    driverLicense,
+  }: UserDTO.User): Promise<void> {
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      password,
+      driverLicense,
+    });
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find((user) => user.email === email);
+  }
+
+  async findByUsername(username: string): Promise<User> {
+    return this.users.find((user) => user.username === username);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+}
