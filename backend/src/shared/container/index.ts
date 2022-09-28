441fe4df5366a687a3647351ee3077d593e9661b
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-28 15:02:28 -0300

    feat: add users repository singleton

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -6,6 +6,9 @@ import { CategoriesRepository } from "../../modules/cars/repositories/implementa
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationRepository";
 
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository
@@ -16,3 +19,7 @@ container.registerSingleton<ISpecificationsRepository>(
   SpecificationsRepository
 );
 
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
+);
