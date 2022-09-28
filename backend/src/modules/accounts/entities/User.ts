28f1040f5343c8503e7eb4edbd1ff868ebebab0f
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-28 14:52:28 -0300

    feat: create user entity

---
+++ backend/src/modules/accounts/entities/User.ts
@@ -0,0 +1,35 @@
+import { v4 as uuidV4 } from "uuid";
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  driverLicense: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
