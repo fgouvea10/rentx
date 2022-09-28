d7b9965170152c4de73224444e634471bdd936dc
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-28 15:06:13 -0300

    feat: add user routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificationRoutes } from "./specifications.routes";
+import { usersRoutes } from "./users.routes";
 
 export const appRoutes = Router();
 
 appRoutes.use("/categories", categoriesRoutes);
 appRoutes.use("/specifications", specificationRoutes);
+appRoutes.use("/users", usersRoutes);
