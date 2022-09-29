c7217a5179b6b92f7a6fbc85a1cb1869b7b547c1
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-29 16:32:49 -0300

    feat: add available cars route

--- backend/src/shared/infra/http/routes/cars.routes.ts
+++ backend/src/shared/infra/http/routes/cars.routes.ts
@@ -3,14 +3,17 @@ import { Router } from "express";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 
 export const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post(
   "/",
   ensureAuthenticated,
-  ensureAdmin,
+  // ensureAdmin,
   createCarController.handle
 );
+carsRoutes.get("/available", listAvailableCarsController.handle);
