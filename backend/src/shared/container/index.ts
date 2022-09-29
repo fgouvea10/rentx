a587568f5f97cc3ada9bedcaac1dadc135d5a786
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-29 12:13:05 -0300

    feat: add car singleton

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -8,6 +8,8 @@ import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecifica
 
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -23,3 +25,5 @@ container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
 );
+
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
