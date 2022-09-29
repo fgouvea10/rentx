b1c5f40a95640ef996cfe34a663fd54177355f23
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-29 12:13:15 -0300

    feat: add cars routes

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { authRoutes } from "./auth.routes";
 
+import { authRoutes } from "./auth.routes";
+import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
@@ -11,3 +12,4 @@ appRoutes.use("/categories", categoriesRoutes);
 appRoutes.use("/specifications", specificationRoutes);
 appRoutes.use("/users", usersRoutes);
 appRoutes.use("/auth", authRoutes);
+appRoutes.use("/cars", carsRoutes);
