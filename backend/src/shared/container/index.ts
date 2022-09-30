1c57f84fd2f1ca3853ce4dec8ef37dd2cca4e8a0
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-30 13:11:19 -0300

    feat: register cars image singleton

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -10,6 +10,8 @@ import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/Us
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -27,3 +29,5 @@ container.registerSingleton<IUsersRepository>(
 );
 
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+container.registerSingleton<ICarsImagesRepository>("CarsImagesRepository", CarsImagesRepository);
