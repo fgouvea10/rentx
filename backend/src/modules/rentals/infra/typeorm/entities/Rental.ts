952586b4e9e038e2d7f000bf44a4facf948fdca3
fgouvea10 <felipegalvesouvea@gmail.com>
2022-09-30 16:44:21 -0300

    feat: add typeorm annotations

--- backend/src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ backend/src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,22 +1,33 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+@Entity('rentals')
 export class Rental {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   carId: string;
 
+  @Column()
   userId: string;
 
+  @Column()
   startDate: Date;
 
+  @Column()
   endDate: Date;
 
+  @Column()
   expectedReturnDate: Date;
 
+  @Column()
   total: number;
 
+  @CreateDateColumn()
   createdAt: Date;
 
+  @UpdateDateColumn()
   updatedAt: Date;
 
   constructor() {
