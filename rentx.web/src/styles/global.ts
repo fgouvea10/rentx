ee3434e8414b4e7da0a45828225a44b341c7deef
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-01 00:04:37 -0300

    feat: add global style

---
+++ rentx.web/src/styles/global.ts
@@ -0,0 +1,58 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+  },
+
+  body: {
+    backgroundColor: "$gray100",
+    color: "$gray900",
+    "-webkit-font-smoothing": "antialiased",
+  },
+
+  html: {
+    fontFamily: "$sans",
+    "-ms-text-size-adjust": "100%",
+    "-webkit-text-size-adjust": "100%",
+  },
+
+  "body, input, textarea, button": {
+    fontFamily: "$base",
+    fontWeight: "$regular",
+    letterSpacing: "-0.06em",
+  },
+
+  "h1, h2, h3, h4, h5, h6": {
+    fontSize: "inherit",
+    fontWeight: "inherit",
+  },
+
+  "ol, ul": {
+    listStyle: "none",
+    margin: 0,
+    padding: 0,
+  },
+
+  "article, aside, details, footer, header, main, nav, section": {
+    display: "block",
+  },
+
+  hr: {
+    "-moz-box-sizing": "content-box",
+    "box-sizing": "content-box",
+    height: 0,
+  },
+
+  button: {
+    border: 0,
+    cursor: "pointer",
+  },
+
+  a: {
+    color: "inherit",
+    textDecoration: "none",
+  },
+});
--- rentx.web/src/styles/index.ts
+++ rentx.web/src/styles/index.ts
@@ -25,35 +25,36 @@ export const {
     },
 
     space: {
-      0: '0px',
-      1: '0.25rem',
-      2: '0.5rem',
-      3: '0.75rem',
-      4: '0.1rem',
-      5: '1.25rem',
-      6: '1.5rem',
-      7: '2rem',
-      8: '2.5rem',
-      12: '3rem',
-      16: '4rem',
-      20: '5rem',
-      40: '7rem',
+      0: "0px",
+      1: "0.25rem",
+      2: "0.5rem",
+      3: "0.75rem",
+      4: "0.1rem",
+      5: "1.25rem",
+      6: "1.5rem",
+      7: "2rem",
+      8: "2.5rem",
+      12: "3rem",
+      16: "4rem",
+      20: "5rem",
+      40: "7rem",
     },
 
     fontSizes: {
-      xs: '0.75rem',
-      sm: '0.875rem',
-      md: '1rem',
-      lg: '1.25rem',
-      xl: '1.5rem',
-      '2xl': '2rem',
-      '3xl': '3rem',
-      '4xl': '4rem',
+      xs: "0.75rem",
+      sm: "0.875rem",
+      md: "1rem",
+      lg: "1.25rem",
+      xl: "1.5rem",
+      "2xl": "2rem",
+      "3xl": "3rem",
+      "4xl": "4rem",
     },
 
     fonts: {
-      base: 'DM Sans, sans-serif',
-      code: 'monospace',
+      sans: "sans-serif",
+      base: "DM Sans, sans-serif",
+      code: "monospace",
     },
 
     fontWeights: {
@@ -61,29 +62,29 @@ export const {
       medium: 500,
       bold: 700,
     },
-  
+
     lineHeights: {
-      shorter: '120%',
-      short: '140%',
-      tall: '160%',
-      taller: '180%',
+      shorter: "120%",
+      short: "140%",
+      tall: "160%",
+      taller: "180%",
     },
 
     radii: {
-      base: '4px',
-      pill: '9999px',
+      base: "4px",
+      pill: "9999px",
     },
 
     borderWidths: {
-      thin: '1px',
-      mid: '2px',
-      thick: '4px',
+      thin: "1px",
+      mid: "2px",
+      thick: "4px",
     },
 
     zIndices: {
-      fixed: '999px',
-      overlay: '9999px',
-      portals: '99999px',
+      fixed: "999px",
+      overlay: "9999px",
+      portals: "99999px",
     },
   },
 });
