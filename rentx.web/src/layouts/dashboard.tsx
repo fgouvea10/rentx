18ea264f3716e1e6b9c504e57b3293ed3d0496e9
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-03 20:08:05 -0300

    feat: add unfinished reservation details screen

--- rentx.web/src/layouts/dashboard.tsx
+++ rentx.web/src/layouts/dashboard.tsx
@@ -1,13 +1,14 @@
 import { AppFooter } from '~/components/layout/footers/dashboard';
 import { AppHeader } from '~/components/layout/headers/Dashboard';
 import { Dashboard } from '~/pages/dashboard/home';
+import { ReservationDetails } from '~/pages/dashboard/reservations/details';
 import { ReservationsList } from '~/pages/dashboard/reservations/list';
 
 export function DashboardLayout() {
   return (
     <>
       <AppHeader />
-      <ReservationsList />
+      <ReservationDetails />
       <AppFooter />
     </>
   );
---
+++ rentx.web/src/pages/dashboard/reservations/details/index.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+export function ReservationDetails() {
+  const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
+
+  return (
+    <main>
+      {/* <div className={styles['img-container']}
+      <img />
+      </div>
+      <div className={styles['reservation-details']}>
+        <small>Audi</small>
+        <p>Q8 2022 2.0</p>
+        <strong><span>R$ 340</span>/dia</strong>
+        {/* <badge>disponível</badge>
+      </div> */}
+      <p>coming...</p>
+    </main>
+  );
+}
