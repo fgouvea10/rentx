0f2616dd754c40d728885407b3bee5d8dadcf7a7
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-03 21:21:40 -0300

    feat: create sign up screen

--- rentx.web/src/routes/auth.routes.tsx
+++ rentx.web/src/routes/auth.routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { SignIn } from '~/pages/auth/signIn';
+import { SignUp } from '~/pages/auth/signUp';
 
 export function AuthRoutes() {
   return (
     <Routes>
       <Route index element={<SignIn />} />
+      <Route path='register' element={<SignUp />} />
     </Routes>
   )
 }
