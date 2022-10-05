e4ce53431bb82da55b5cdc42ca7d7227314d7ca4
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-05 14:11:26 -0300

    feat: global font

--- rentx.admin/tailwind.config.cjs
+++ rentx.admin/tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/components/layout/**/**/*.tsx',
+    './src/pages/**/*.tsx',
+    './src/components/shared/**/*.tsx',
+    './src/components/shared/Form/**/*.tsx',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary400: '#37FB89',
+        primary500: '#01CC56',
+        primary600: '#00A947',
+        red500: '#DC1637',
+        black: '#121214',
+      },
+      fontFamily: {
+        sans: 'DM Sans, sans-serif',
+      },
+      borderRadius: {
+        base: '4px',
+      },
+    },
   },
   plugins: [],
-}
+};
