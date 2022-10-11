f188816974d656d9f0937442310cfc2eac214e10
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-11 12:14:22 -0300

    chore: add react-router-dom

--- rentx.admin/src/layouts/default-layout.tsx
+++ rentx.admin/src/layouts/default-layout.tsx
@@ -2,8 +2,12 @@ import { Header } from "../components/layout/Header"
 import { Sidebar } from "../components/layout/Sidebar";
 import { EditCar } from "../pages/cars/edit";
 import { NewCar } from "../pages/cars/new";
+import { NewCategory } from "../pages/categories/new";
+import { NewCustomer } from "../pages/customers/new";
 import { Dashboard } from "../pages/dashboard";
 import { ListRentals } from "../pages/rentals/list";
+import { EditSpecification } from "../pages/specifications/edit";
+import { NewSpecification } from "../pages/specifications/new";
 
 export function DefaultLayout() {
   return (
@@ -12,7 +16,7 @@ export function DefaultLayout() {
       <div className='flex flex-col w-full'>
         <Header />
         <div className='min-h-screen flex-1 p-6 lg:p-7 lg:px-16 lg:ml-[16rem]'>
-          <EditCar />
+          <NewCategory />
         </div>
       </div>
     </main>
