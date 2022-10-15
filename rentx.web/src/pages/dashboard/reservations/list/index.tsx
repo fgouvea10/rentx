19751134201e8f9e2921a637d6be44c7440bdecf
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-15 00:02:53 -0300

    feat: create user rentals service

--- rentx.web/src/pages/dashboard/reservations/list/index.tsx
+++ rentx.web/src/pages/dashboard/reservations/list/index.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from 'react';
+
+import { getRentalsByUser } from '~/services/useCases/rentals/list-by-user';
+
 import styles from './ReservationsList.module.css';
 
+type Rental = {
+  name: string;
+  brand: string;
+  price: number;
+};
+
 export function ReservationsList() {
-  const isAppointmentActive = false;
+  const [rentals, setRentals] = useState<Rental[]>([]);
+  const [isFetchingRentals, setIsFetchingRentals] = useState(false);
+
+  const listRentals = async () => {
+    setIsFetchingRentals(true);
+
+    try {
+      const response = await getRentalsByUser();
+      setRentals(response);
+    } catch (err) {
+      // console.log('err', err);
+    } finally {
+      setIsFetchingRentals(false);
+    }
+  };
+
+  useEffect(() => {
+    listRentals();
+  }, []);
 
   return (
     <main className={styles.main}>
@@ -16,41 +44,54 @@ export function ReservationsList() {
       <section className={styles.section}>
         <div className={styles.container}>
           <div className={styles['grid-container']}>
-            {[...Array(12)].map((_, index) => (
-              <a
-                key={index}
-                href="/reservas/as86uja-gahb8s-asgbv7"
-                className={`${styles.card} relative`}
-              >
-                <div className={styles['reservation-card-container']}>
-                  <div className="flex flex-col gap-4 justify-between">
-                    <div>
-                      <small className={styles['info-car-text']}>Porsche</small>
-                      <span className={styles['car-name']}>Random porsche</span>
+            {rentals && rentals.length > 0 ? (
+              rentals.map((rental) => (
+                <a
+                  key={rental.name}
+                  href="/reservas/as86uja-gahb8s-asgbv7"
+                  className={`${styles.card} relative`}
+                >
+                  <div className={styles['reservation-card-container']}>
+                    <div className="flex flex-col gap-4 justify-between">
+                      <div>
+                        <small className={styles['info-car-text']}>
+                          {rental.brand}
+                        </small>
+                        <span className={styles['car-name']}>
+                          {rental.name}
+                        </span>
+                      </div>
+                      <div>
+                        <small className={styles['info-car-text']}>
+                          Ao dia
+                        </small>
+                        <span className={styles['car-daily-price']}>
+                          R$ {rental.price}
+                        </span>
+                      </div>
                     </div>
-                    <div>
-                      <small className={styles['info-car-text']}>Ao dia</small>
-                      <span className={styles['car-daily-price']}>R$ 340</span>
-                    </div>
+                    <img
+                      src="https://www.picng.com/upload/porsche/png_porsche_22652.png"
+                      alt=""
+                    />
                   </div>
-                  <img
-                    src="https://www.picng.com/upload/porsche/png_porsche_22652.png"
-                    alt=""
-                  />
-                </div>
-                {isAppointmentActive && (
-                  <>
-                    <div
-                      className={styles['active-reservation-animated']}
-                    ></div>
-                    <div
-                      title="Reserva em andamento"
-                      className={styles['active-reservation']}
-                    ></div>
-                  </>
-                )}
-              </a>
-            ))}
+                  {/* {isAppointmentActive && (
+                <>
+                  <div
+                    className={styles['active-reservation-animated']}
+                  ></div>
+                  <div
+                    title="Reserva em andamento"
+                    className={styles['active-reservation']}
+                  ></div>
+                </>
+              )} */}
+                </a>
+              ))
+            ) : (
+              <p>Você não possui registro de reservas</p>
+              // TODO: better style for empty rentals
+            )}
           </div>
         </div>
       </section>
