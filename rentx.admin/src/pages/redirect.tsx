a63d2c01efb35231de9d93c29058804cf7be50ee
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-11-09 18:17:27 -0300

    refactor: add verify user function in redirect

--- rentx.admin/src/pages/redirect.tsx
+++ rentx.admin/src/pages/redirect.tsx
@@ -1,31 +1,16 @@
-import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-import { getUser } from "../services/useCases/getUser";
+import { AuthContext } from "../contexts/AuthContext";
 
 export function Redirect() {
   const location = useLocation();
-  const navigate = useNavigate();
+  const { verifyIfUserIsAdmin } = useContext(AuthContext);
 
   const [, token] = location.search.split("=");
 
-  async function verifyUser() {
-    try {
-      const response = await getUser({ token });
-      if (!response.isAdmin)
-        return (window.location.href = `${
-          import.meta.env.VITE_RENTX_WEB_URL
-        }/auth`);
-
-      navigate("/");
-    } catch (err) {
-      console.log("failed err", err);
-      window.location.href = `${import.meta.env.VITE_RENTX_WEB_URL}/auth`;
-    }
-  }
-
   useEffect(() => {
-    verifyUser();
+    verifyIfUserIsAdmin({ token });
   }, []);
 
   return (
