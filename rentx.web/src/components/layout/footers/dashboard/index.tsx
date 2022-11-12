7b0196668085c78ae2407b64fab9b46bf4455d11
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-11-12 19:34:23 -0300

    feat: add footer identation

--- rentx.web/src/components/layout/footers/dashboard/index.tsx
+++ rentx.web/src/components/layout/footers/dashboard/index.tsx
@@ -9,33 +9,33 @@ export function AppFooter(): ReactElement {
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles['flex-column']}>
-        <img src={logoImg} alt="" />
-        <div className={styles['links-container']}>
-          <div>
-            <strong>Links rápidos</strong>
-            <ul>
-              <li>Página principal</li>
-              <li>Trabalhe conosco</li>
-              <li>Aplicativo RentX</li>
-            </ul>
+          <img src={logoImg} alt="" />
+          <div className={styles['links-container']}>
+            <div>
+              <strong>Links rápidos</strong>
+              <ul>
+                <li>Página principal</li>
+                <li>Trabalhe conosco</li>
+                <li>Aplicativo RentX</li>
+              </ul>
+            </div>
+            <div>
+              <strong>Redes sociais</strong>
+              <ul>
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>Github</li>
+              </ul>
+            </div>
+            <div>
+              <strong>Legal</strong>
+              <ul>
+                <li>Política de Privacidade</li>
+                <li>Termos de uso</li>
+                <li>Cookies</li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <strong>Redes sociais</strong>
-            <ul>
-              <li>Facebook</li>
-              <li>Instagram</li>
-              <li>Github</li>
-            </ul>
-          </div>
-          <div>
-            <strong>Legal</strong>
-            <ul>
-              <li>Política de Privacidade</li>
-              <li>Termos de uso</li>
-              <li>Cookies</li>
-            </ul>
-          </div>
-        </div>
         </div>
         <div className={styles.copy}>
           <p>&copy; 2022 RentX - Todos os direitos reservados.</p>
