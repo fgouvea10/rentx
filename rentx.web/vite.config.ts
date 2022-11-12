b09ab36b2bc0683d39a199767475e7b91d622e04
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-11-12 18:36:28 -0300

    feat: add docker setup in web application

--- rentx.web/vite.config.ts
+++ rentx.web/vite.config.ts
@@ -4,6 +4,10 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   plugins: [react()],
   resolve: {
     alias: {
