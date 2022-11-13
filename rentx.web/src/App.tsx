d38f4cdd65043ebc65ab04a56d20524af5ee38c1
Felipe Gouvea <felipegalvesouvea@gmail.com>
2022-11-13 17:26:52 -0300

    chore: add react date range

--- rentx.web/src/App.tsx
+++ rentx.web/src/App.tsx
@@ -3,11 +3,13 @@ import { Routes } from './routes';
 import { AuthProvider } from './contexts/AuthContext';
 
 import '~/styles/global.css';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 
 export function App() {
   return (
     <AuthProvider>
-        <Routes />
+      <Routes />
     </AuthProvider>
   );
 }
